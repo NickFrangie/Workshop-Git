ba5fa99f60ae596cc39a09273bb44568b91279ca
NickFrangie <51765298+NickFrangie@users.noreply.github.com>
2024-02-22 16:52:14 -0500

    Add Values function.

--- Workshop-Git/main.cpp
+++ Workshop-Git/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
+int addValues(int a, int b)
+{
+    return 0;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "My name is {Your Name Here}!\n";
     std::cout << "Today is {Date Here}!\n";
+
+    int a = 2;
+    int b = 7;
+    std::cout << "Adding these numbers equals... " << addValues(a, b) << "\n";
 }
